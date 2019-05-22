600983279825cd279b3186c38384fead4f8ba113
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-22 21:43:39 +0200

    finished the first part of day 8 in a linear fashion

--- 2018/8-1.py
+++ 2018/8-1.py
@@ -5,8 +5,9 @@ import sys
 
 #read input
 with open('8-input.txt','r') as f:
-	#content = """2 3 0 3 10 11 12 2 1 1 1 0 1 5 99 0 1 2 2 1 1 2""".split(' ')#f.read().split(' ')
-	content = """2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2""".split(' ')
+	content = f.read().split(' ')
+	#content = """2 3 0 3 10 11 12 2 1 1 1 0 1 5 99 0 1 2 2 1 1 2""".split(' ')
+	#content = """2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2""".split(' ')
 
 content = [int(x) for x in content]
 
@@ -20,18 +21,40 @@ class Tree():
 
 	def __init__(self,key):
 		self.processed = False
+		self.key = key
 		self.current_depth = 0
 		self.current_index = 2
-		self.children_list = [key[0]]
-		self.metadata_list = [key[1]]
+		self.children_list = [self.key[0]]
+		self.metadata_list = [self.key[1]]
+		self.metadata_sum = 0
 		
 	def walk(self):
-		raise NotImplementedError
+		if self.children_list[-1] > 0:
+			self.current_depth += 1
+			self.children_list.append(self.key[self.current_index])
+			self.current_index += 1
+			self.metadata_list.append(self.key[self.current_index])
+		elif self.children_list[-1] == 0:
+			if self.metadata_list[-1] > 0:
+				print("adding %s to metadata"%self.key[self.current_index])
+				self.metadata_sum += self.key[self.current_index]
+				self.metadata_list[-1] -= 1
+			if self.metadata_list[-1] == 0:
+				self.current_depth -= 1
+				del self.children_list[-1]
+				del self.metadata_list[-1]
+				if len(self.children_list) > 0:
+					self.children_list[-1] -= 1
+		self.current_index += 1
 
+		if self.current_index >= len(self.key):
+			self.processed = True
 
 tree = Tree(content)
 
 while not tree.processed:
+	print("last value: %s, current index: %s, depth: %s, metadata sum: %s"%(tree.key[tree.current_index-1],tree.current_index,tree.current_depth,tree.metadata_sum))
 	tree.walk()
 
-print(tree.metadata_sum)
+print("last value: %s, current index: %s, depth: %s, metadata sum: %s"%(tree.key[tree.current_index-1],tree.current_index,tree.current_depth,tree.metadata_sum))
+print("final metadata sum: %s"%tree.metadata_sum)
