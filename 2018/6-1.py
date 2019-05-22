70896f4ef37553f8eea1b71999413ef7da6c48da
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-22 20:32:57 +0200

    sixth day

---
+++ 2018/6-1.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+
+import os
+import numpy as np
+import pandas as pd
+import re
+
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+
+#read input
+with open('6-input.txt','r') as f:
+	content = f.read().split('\n')
+
+content_tuples = []
+
+for line in content:
+	content_tuples.append(tuple(line.split(', ')))
+
+content_tuples = [(int(x),int(y)) for x,y in content_tuples]
+
+# 400x400 should be enough for our purposes
+
+df = pd.DataFrame([[[] for x in range(400)] for y in range(400)])
+
+for y in range(400):
+	for x in range(400):
+		distances = [(tup,abs(tup[0]-x)+abs(tup[1]-y)) for tup in content_tuples]
+		min_distance = min(distances,key=lambda x:x[1])
+		df.iat[x,y] = tuple(tup for tup,distance in distances if distance == min_distance[1])
+
+counts = []
+
+for series in df.columns:
+	counts.append(df[series].value_counts())
+
+new_df = pd.DataFrame(pd.concat(counts,axis=0))
+
+values_to_drop = set(df[0].value_counts().index.tolist()+df[399].value_counts().index.tolist()+df.iloc[0].value_counts().index.tolist()+df.iloc[399].value_counts().index.tolist())
+
+new_df.drop(list(values_to_drop),inplace=True)
+new_df.reset_index(drop=False,inplace=True)
+new_df.columns = ['index','counts']
+
+totals = new_df.groupby('index').agg({'counts':'sum'}).sort_values(by='counts',ascending=False)
+
+print(totals)
---
+++ 2018/6-2.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import os
+import numpy as np
+import pandas as pd
+import re
+
+pd.options.display.max_rows = 999
+pd.options.display.max_columns = 999
+
+#read input
+with open('6-input.txt','r') as f:
+	content = f.read().split('\n')
+
+content_tuples = []
+
+for line in content:
+	content_tuples.append(tuple(line.split(', ')))
+
+content_tuples = [(int(x),int(y)) for x,y in content_tuples]
+
+# 400x400 should be enough for our purposes
+
+df = pd.DataFrame([[[] for x in range(400)] for y in range(400)])
+
+for y in range(400):
+	for x in range(400):
+		distances = [(abs(tup[0]-x)+abs(tup[1]-y)) for tup in content_tuples]
+		summed_distance = sum(distances)
+		df.iat[x,y] = True if summed_distance < 10000 else False
+
+counts = []
+
+for series in df.columns:
+	counts.append(df[series].value_counts())
+
+new_df = pd.DataFrame(pd.concat(counts,axis=0))
+
+new_df.reset_index(drop=False,inplace=True)
+new_df.columns = ['index','counts']
+
+totals = new_df.groupby('index').agg({'counts':'sum'}).sort_values(by='counts',ascending=False)
+
+print(totals)
