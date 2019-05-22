1b2741fe4c87a0540c8c08293bccf04563242726
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-22 21:16:48 +0200

    concept for solving this puzzle in a linear fashion instead of recursive

--- 2018/8-1.py
+++ 2018/8-1.py
@@ -3,42 +3,35 @@
 import os
 import sys
 
-sys.setrecursionlimit(5000)
-
 #read input
 with open('8-input.txt','r') as f:
-	content = f.read().split(' ')
+	#content = """2 3 0 3 10 11 12 2 1 1 1 0 1 5 99 0 1 2 2 1 1 2""".split(' ')#f.read().split(' ')
+	content = """2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2""".split(' ')
 
 content = [int(x) for x in content]
 
 #header has two numbers exactly: # nodes, # of metadata entries.
-#idea is to use recursion to calculate this
+#original idea was to use recursion to calculate this, but max recursion depth hit and python doesn't support TCO
 
-def find_subtrees(license_key,start_location=0):
-	header = license_key[start_location:start_location+2] #first two numbers
-	subnode_count = header[0]
-	metadata_count = header[1]
-	print('node header: %s, %s' % (subnode_count,metadata_count))
+"""[10:39 AM] Ves Zappa: Wait, I've looked more closely at my solution. I think the first part is no longer recursive
+[10:43 AM] Ves Zappa: So, what I did was create a Node class that keeps track of the number of children and the number of children we've processed and the value of children. That way we can walk through the data in a linear fashion and we know whether or not we we're processing metadata (the current node has no more unprocessed children) or busy with processing children (there are children)"""
 
-	if subnode_count == 0:
-		# print('returning: %s %s'% (sum(license_key[2:2+metadata_count]),metadata_count+2))
-		return sum(license_key[2:2+metadata_count]),metadata_count+2
+class Tree():
 
-	body = license_key[start_location+2:]
-	metadata_sum = 0
-	progress = 0
+	def __init__(self,key):
+		self.processed = False
+		self.current_depth = 0
+		self.current_index = 2
+		self.children_list = [key[0]]
+		self.metadata_list = [key[1]]
+		
+	def walk(self):
+		raise NotImplementedError
 
-	for subnode in range(subnode_count):
-		new_metadata,new_progress = find_subtrees(body,progress)
-		metadata_sum += new_metadata
-		progress += new_progress
 
-	# print('in node: %s, %s' % (subnode_count,metadata_count))
-	# print('body, prog: %s %s'%(body,progress))
-	metadata_sum += sum(body[progress:progress+metadata_count])
+tree = Tree(content)
 
-	return metadata_sum,progress+2
+while not tree.processed:
+	tree.walk()
 
-metadata_sum,_ = find_subtrees(content)
-
-print('total metadata: %s' % metadata_sum)
+print(tree.metadata_sum)
