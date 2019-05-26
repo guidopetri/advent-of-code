c35ee9e2853aa52b6d772d2482f62d1aaffc7c2e
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-26 18:12:04 +0200

    pep8 fixes

--- 2016/3-1.py
+++ 2016/3-1.py
@@ -1,24 +1,29 @@
 #! /usr/bin/env python
 
-import os
-import sys
 import re
 
-with open('3-1-input.txt','r') as f:
-	data = f.read().split('\n')
+
+def valid_triangle(num1, num2, num3):
+    s1 = num2 + num3
+    s2 = num1 + num3
+    s3 = num1 + num2
+
+    return ((s1 > num1) & (s2 > num2) & (s3 > num3))
+
+
+with open('3-1-input.txt', 'r') as f:
+    data = f.read().split('\n')
 
 valid_triangles = 0
 
 for line in data:
-	if len(line) > 3:
-		numbers = re.search(r'(\d+) +(\d+) +(\d+)',line)
-		first_num = int(numbers.group(1))
-		second_num = int(numbers.group(2))
-		third_num = int(numbers.group(3))
+    if len(line) > 3:
+        numbers = re.search(r'(\d+) +(\d+) +(\d+)', line)
+        first_num = int(numbers.group(1))
+        second_num = int(numbers.group(2))
+        third_num = int(numbers.group(3))
 
-		if ((first_num + second_num > third_num) &
-			(first_num + third_num > second_num) &
-			(second_num + third_num > first_num)):
-			valid_triangles += 1
+        if valid_triangle(first_num, second_num, third_num):
+            valid_triangles += 1
 
 print("Valid triangles: {}".format(valid_triangles))
--- 2016/3-2.py
+++ 2016/3-2.py
@@ -1,26 +1,36 @@
 #! /usr/bin/env python
 
-import os
-import sys
 import re
 
-with open('3-1-input.txt','r') as f:
-	data = f.read()
+
+def valid_triangle(num1, num2, num3):
+    s1 = num2 + num3
+    s2 = num1 + num3
+    s3 = num1 + num2
+
+    return ((s1 > num1) & (s2 > num2) & (s3 > num3))
+
+
+with open('3-1-input.txt', 'r') as f:
+    data = f.read()
 
 valid_triangles = 0
 
-for lines in re.finditer(r'.+?\n.+?\n.+?\n',data):
-	line_list = lines.group(0).split('\n')
-	line_list = [x for x in line_list if len(x) > 1]
-	line_groups = [re.search(r'(\d+) +(\d+) +(\d+)',line) for line in line_list]
-	triangles = [[x.group(y) for x in line_groups] for y in range(1,4)]
-	for triangle in triangles:
-		first_num = int(triangle[0])
-		second_num = int(triangle[1])
-		third_num = int(triangle[2])
-		if ((first_num + second_num > third_num) &
-			(first_num + third_num > second_num) &
-			(second_num + third_num > first_num)):
-			valid_triangles += 1
+for lines in re.finditer(r'.+?\n.+?\n.+?\n', data):
+    line_list = lines.group(0).split('\n')
+    line_list = [x for x in line_list if len(x) > 1]
+
+    line_groups = []
+    for line in line_list:
+        line_groups.append(re.search(r'(\d+) +(\d+) +(\d+)', line))
+
+    triangles = [[x.group(y) for x in line_groups] for y in range(1, 4)]
+    for triangle in triangles:
+        first_num = int(triangle[0])
+        second_num = int(triangle[1])
+        third_num = int(triangle[2])
+
+        if valid_triangle(first_num, second_num, third_num):
+            valid_triangles += 1
 
 print("Valid triangles: {}".format(valid_triangles))
