751bce10544b942008032559d6ef8c8015a74ee4
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-26 23:54:17 +0200

    solve day 10 part 1 and 2 all at once, accidentally

--- 2018/10-1.py
+++ 2018/10-1.py
@@ -1,22 +1,27 @@
 #! /usr/bin/env python
 
 import re
+from collections import OrderedDict
 
 
 class Point():
 
     def __init__(self, position, velocity):
         self.position = position
-        self.velocity = velocity
+        self.velo = velocity
 
     def move_ahead(self):
-        self.position = (sum(x) for x in zip(self.position, self.velocity))
+        self.position = tuple(sum(x) for x in zip(self.position, self.velo))
+
+    def move_behind(self):
+        neg_velo = (-x for x in self.velo)
+        self.position = tuple(sum(x) for x in zip(self.position, neg_velo))
 
     def get_position(self):
         return self.position
 
     def get_velocity(self):
-        return self.velocity
+        return self.velo
 
     def __repr__(self):
         pos = self.get_position()
@@ -31,6 +36,53 @@ def find_point(line):
     velocity = (int(match.group(3)), int(match.group(4)))
     new_point = Point(position, velocity)
     return new_point
+
+
+def get_furthest_positions(points):
+    max_x = 0
+    max_y = 0
+
+    min_x = 0
+    min_y = 0
+
+    positions = [point.get_position() for point in points]
+
+    max_x = max(positions, key=lambda x: x[0])[0]
+    max_y = max(positions, key=lambda x: x[1])[1]
+
+    min_x = min(positions, key=lambda x: x[0])[0]
+    min_y = min(positions, key=lambda x: x[1])[1]
+    return max_x, max_y, min_x, min_y
+
+
+def compose_image(points):
+    max_x, max_y, min_x, min_y = get_furthest_positions(points)
+
+    lines = OrderedDict()
+    for y in range(min_y, max_y + 1):
+        lines[y] = OrderedDict()
+        for x in range(min_x, max_x + 1):
+            cell = '.'
+            lines[y][x] = cell
+
+    for point in points:
+        pos = point.get_position()
+
+        lines[pos[1]][pos[0]] = '#'
+
+    newlines = [''.join(line.values()) for line in lines.values()]
+    image = '\n'.join(newlines)
+
+    return image
+
+
+def calculate_distance(points):
+    max_x, max_y, min_x, min_y = get_furthest_positions(points)
+
+    distance_x = abs(max_x - min_x)
+    distance_y = abs(max_y - min_y)
+
+    return distance_x + distance_y
 
 
 with open('10-input.txt', 'r') as f:
@@ -70,7 +122,27 @@ position=<-3,  6> velocity=< 2, -1>""".split('\n')
 
 points = []
 
-for line in example_content:
+for line in content:
     points.append(find_point(line))
 
-print(points)
+loopcount = 0
+distance = calculate_distance(points)
+last_distance = distance + 1
+while distance < last_distance:
+    if loopcount % 1000 == 0:
+        print("looped {} times. distance is {}".format(loopcount, distance))
+    if loopcount > 200000:
+        print("early break")
+        break
+    loopcount += 1
+    [point.move_ahead() for point in points]
+    last_distance = distance
+    distance = calculate_distance(points)
+
+loopcount -= 1
+[point.move_behind() for point in points]
+
+print("Took {} seconds. Smallest distance is {}".format(loopcount, distance))
+
+ascii_image = compose_image(points)
+print(ascii_image)
