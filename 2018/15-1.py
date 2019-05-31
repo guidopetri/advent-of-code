2bf85370d984a17dcee340f99a3405f82aa077fd
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-05-31 22:39:12 +0200

    implement hp, atk, class counters; implement attack; small while loop with simulation logic

--- 2018/15-1.py
+++ 2018/15-1.py
@@ -20,6 +20,13 @@ class BattleUnit(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.atk = 3
+        self.hp = 200
+        self.add_to_counter(self)
+
+    @classmethod
+    def add_to_counter(cls, self):
+        cls.units.append(self)
 
     @property
     def position(self):
@@ -29,8 +36,24 @@ class BattleUnit(object):
         return pos
 
     @property
-    def team(self):
+    def team(cls):
         pass  # default implementation
+
+    def attack(self, units):
+        # print('attacking, {}'.format(self))
+        attackable = []
+        for unit in units:
+            if get_distance(self, unit) == 1:
+                attackable.append(unit)
+        if attackable:
+            if len(attackable) > 1:
+                attackable.sort(key=lambda x: x.hp)
+
+            other = attackable[0]
+            other.hp -= self.atk
+
+    def move(self, units):
+        pass
 
     def __repr__(self):
         return "{} unit: {}".format(self.team, self.position)
@@ -38,12 +61,16 @@ class BattleUnit(object):
 
 class Goblin(BattleUnit):
 
+    units = []
+
     @property
     def team(self):
         return "Goblin"
 
 
 class Elf(BattleUnit):
+
+    units = []
 
     @property
     def team(self):
@@ -56,7 +83,21 @@ def find_order(units):
         return priority
 
     units.sort(key=read_order_priority)
-    return units
+    return
+
+
+def find_nearest(unit_pos, other_pos):
+    distances = {}
+    for pos in other_pos:
+        distances[pos] = get_distance(unit_pos, pos)
+
+    min_distance = min(distances.values())
+
+    return [pos for pos in other_pos if distances[pos] == min_distance]
+
+
+def get_distance(pos1, pos2):
+    return abs(pos1.x - pos2.x) + abs(pos1.y - pos2.y)
 
 
 with open('15-input.txt', 'r') as f:
@@ -78,4 +119,12 @@ for line in raw_data.split('\n'):
         units.append(new_unit)
     y += 1
 
-print(units)
+while Goblin.units and Elf.units:
+    find_order(units)
+    for unit in units:
+        unit.move(units)
+        unit.attack(units)
+    break
+
+print(Goblin.units)
+# print(Elf.units)
