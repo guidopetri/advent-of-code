87d3de451fe7a1f0969a2ed44c4d50500cab6a63
Guido (Sid) Petri <18634426+guidopetri@users.noreply.github.com>
2022-12-04 10:19:56 -0600

    4th day of 2022

---
+++ 2022/src/04-2.rs
@@ -0,0 +1,22 @@
+use std::fs;
+
+fn main() {
+    let file_path = "04-input.txt";
+    let raw_input = fs::read_to_string(file_path).expect("Could not read file {file_path}");
+    let lines: Vec<String> = raw_input.as_str().trim().split("\n").map(|s| s.to_string()).collect();
+
+    let mut overlapping: u32 = 0;
+
+    for line in lines {
+        let sections: Vec<String> = line.as_str().split(",").map(|s| s.to_string()).collect();
+        let first_section_lims: Vec<u32> = sections[0].as_str().split("-").map(|s| s.parse::<u32>().unwrap()).collect();
+        let second_section_lims: Vec<u32> = sections[1].as_str().split("-").map(|s| s.parse::<u32>().unwrap()).collect();
+
+        if ((first_section_lims[1] >= second_section_lims[0]))
+          & ((second_section_lims[1] >= first_section_lims[0])) {
+            overlapping += 1;
+          }
+    }
+
+    println!("{overlapping:?}");
+}
